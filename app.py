60f81cedf32a2699e9a8153e8e9f2caca6c0a7c6
Mayya <mayya.mukhina@icloud.com>
2022-05-09 09:02:57 +0300

    change

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for, request, render_template,redirect, send_from_directory, make_response, flash
+# Подгружаем все необходимые библиотеки
+from flask import Flask, url_for, request, render_template, redirect, send_from_directory, make_response, flash
 import json
 import psycopg2
 from flask_wtf import FlaskForm
@@ -9,6 +10,7 @@ from werkzeug.utils import secure_filename
 import urllib.request
 import os
 
+# Присваиваем значения константам
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -18,61 +20,71 @@ app.config['SECRET_KEY'] = 'secret_key'
 socketio = SocketIO(app)
 DATABASE_URL = os.environ['DATABASE_URL']
 
-black_orig = {"A1":"", "A2": "", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "pawnb", "A8": "rookb",
-         "B1":"", "B2": "", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "pawnb", "B8": "knightb",
-         "C1":"", "C2": "", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "pawnb", "C8": "bishopb",
-         "D1":"", "D2": "", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "pawnb", "D8": "queenb",
-         "E1":"", "E2": "", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "pawnb", "E8": "kingb",
-         "F1":"", "F2": "", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "pawnb", "F8": "bishopb",
-         "G1":"", "G2": "", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "pawnb", "G8": "knightb",
-         "H1":"", "H2": "", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "pawnb", "H8": "rookb"}
+black_orig = {"A1": "", "A2": "", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "pawnb", "A8": "rookb",
+         "B1": "", "B2": "", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "pawnb", "B8": "knightb",
+         "C1": "", "C2": "", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "pawnb", "C8": "bishopb",
+         "D1": "", "D2": "", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "pawnb", "D8": "queenb",
+         "E1": "", "E2": "", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "pawnb", "E8": "kingb",
+         "F1": "", "F2": "", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "pawnb", "F8": "bishopb",
+         "G1": "", "G2": "", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "pawnb", "G8": "knightb",
+         "H1": "", "H2": "", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "pawnb", "H8": "rookb"}
 
-white_orig = {"A1":"rookw", "A2": "pawnw", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "", "A8": "",
-         "B1":"knightw", "B2": "pawnw", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "", "B8": "",
-         "C1":"bishopw", "C2": "pawnw", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "", "C8": "",
-         "D1":"queenw", "D2": "pawnw", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "", "D8": "",
-         "E1":"kingw", "E2": "pawnw", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "", "E8": "",
-         "F1":"bishopw", "F2": "pawnw", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "", "F8": "",
-         "G1":"knightw", "G2": "pawnw", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "", "G8": "",
-         "H1":"rookw", "H2": "pawnw", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "", "H8": ""}
+white_orig = {"A1": "rookw", "A2": "pawnw", "A3": "", "A4": "", "A5": "", "A6": "", "A7": "", "A8": "",
+         "B1": "knightw", "B2": "pawnw", "B3": "", "B4": "", "B5": "", "B6": "", "B7": "", "B8": "",
+         "C1": "bishopw", "C2": "pawnw", "C3": "", "C4": "", "C5": "", "C6": "", "C7": "", "C8": "",
+         "D1": "queenw", "D2": "pawnw", "D3": "", "D4": "", "D5": "", "D6": "", "D7": "", "D8": "",
+         "E1": "kingw", "E2": "pawnw", "E3": "", "E4": "", "E5": "", "E6": "", "E7": "", "E8": "",
+         "F1": "bishopw", "F2": "pawnw", "F3": "", "F4": "", "F5": "", "F6": "", "F7": "", "F8": "",
+         "G1": "knightw", "G2": "pawnw", "G3": "", "G4": "", "G5": "", "G6": "", "G7": "", "G8": "",
+         "H1": "rookw", "H2": "pawnw", "H3": "", "H4": "", "H5": "", "H6": "", "H7": "", "H8": ""}
 
 
+# Форма для логина
 class LoginForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(message="Введите логин")])
     password = PasswordField('Пароль', validators=[DataRequired(message="Введите пароль")])
     submit = SubmitField('Войти')
 
 
+# Функция для проверки занят ли логие при регистрации
 def my_check(form, field):
     database_list = database("register")
     if field.data in database_list:
         raise ValidationError("Этот логин уже занят!")
 
+
+# Функция для проверки ID при создании новой игры
 def my_check_profile(form, field):
     database_list = [str(i) for i in database("available")]
     if (field.data not in database_list) or (int(field.data) == 1):
         raise ValidationError("Такой ID не существует!")
-    
+
+
+# Форма для регистрации
 class RegisterForm(FlaskForm):
     username = StringField('Логин', validators=[DataRequired(message="Введите логин"), my_check])
     password = PasswordField('Пароль', validators=[DataRequired(message="Введите пароль")])
-    password_rep = PasswordField('Повторите пароль', validators=[DataRequired(message="Введите пароль"),EqualTo('password', message='Пароли не совпадают')])
+    password_rep = PasswordField('Повторите пароль', validators=[DataRequired(message="Введите пароль"), EqualTo('password', message='Пароли не совпадают')])
     checkbox = BooleanField("Я соглашаюсь на обработку персональных данных", validators=[DataRequired(message="Необходимое поле")])
     submit = SubmitField('Войти')
 
 
+# Форма для создании игры
 class Search(FlaskForm):
     username = StringField('ID: ', validators=[DataRequired(message="Введите id"), my_check_profile])
     submit = SubmitField('Создать игру')
-    
 
+
+# Форма для закрытии игры
 class Game(FlaskForm):
     end_game = SubmitField("Закончить игру")
     win = SubmitField("Мат")
     draw = SubmitField("Пат")
-    
 
+
+# Функция для получения данных из базы
 def database(state, info=None):
+    # Пробуем подключиться к базе
     try:
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor()
@@ -83,7 +95,7 @@ def database(state, info=None):
         cur.execute("SELECT name FROM users")
         temp = cur.fetchall()
         result = [i[0] for i in temp]
-        
+
     elif state == "login":
         cur.execute("SELECT name, password FROM users WHERE name = '{}';".format(info[0]))
         result = cur.fetchall()
@@ -93,13 +105,13 @@ def database(state, info=None):
             cur.execute("UPDATE users SET picture = 'default.png' WHERE name = '{}'".format(info[0]))
             conn.commit()
 
-    elif state == "insert": 
+    elif state == "insert":
         cur.execute("INSERT INTO users(name, password, picture) VALUES ('{}', '{}', 'default.png')".format(info[0], info[1]))
         conn.commit()
 
         cur.execute("SELECT id FROM users WHERE name = '{}';".format(info[0]))
         id_user = cur.fetchall()[0][0]
-        
+
         cur.execute("INSERT INTO statistic (user_id) VALUES ({})".format(id_user))
         conn.commit()
         result = "success"
@@ -173,18 +185,13 @@ def database(state, info=None):
         cur.execute("SELECT * FROM moves")
         temp = cur.fetchall()
         result = [i[0] for i in temp]
-        
+
     elif state == "error":
         result = "no_in"
 
-        
     cur.close()
-
     conn.close()
-
     return result
-    
-    
 
 
 @app.route('/')
@@ -196,10 +203,12 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # Получаем введенные данные
         username = request.form['username']
         password = request.form['password']
         state = "login"
-        
+
+        # Проверка существуют ли такой логие и пароль
         check = database(state, [username])
         if check == "no_in":
             form.password.errors.append("Проверьте подключение к интернетy")
@@ -217,20 +226,19 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        # Получаем введенные данные
         username = request.form['username']
         password = request.form['password']
 
+        # Сохраняем новые данные пользователя в базу
         insert = database("insert", [username, password])
-        
+
         if insert == "no_in":
             form.password.errors.append("Проверьте подключение к интернетy")
         elif insert == "success":
             return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
-
-
-
 
 
 @app.route('/main/<info>', methods=['GET', 'POST'])
@@ -241,11 +249,13 @@ def main(info):
     black_lis = ["kingb", "queenb", "pawnb", "bishopb", "rookb", "knightb"]
     white_lis = ["kingw", "queenw", "pawnw", "bishopw", "rookw", "knightw"]
 
+    # Создаем копию словаря с изночальными позициями фишками
     black = black_orig.copy()
     white = white_orig.copy()
-                                
+
+    # Меняем позиции фишек если игра не закончена
     for i in moves:
-        temp = i.split(" ")
+        temp = i.strip().split(" ")
         if temp[0] == "d":
             try:
                 white[temp[1]] = ""
@@ -264,8 +274,8 @@ def main(info):
                         white[temp[1]] = temp[2]
                     except Exception:
                         pass
-                    
-                
+
+    # При закрытии игры удаляем из базы и обновляем статистику
     if request.method == 'POST':
         if form.end_game.data:
             close = database("close")
@@ -280,8 +290,7 @@ def main(info):
             if close != "closed":
                 add_draw = database("add_draw", info)
             return redirect(url_for('profile', username=info[2]))
-        
-            
+
     return render_template('main.html', black=black, white=white, player=info[0], apponent=info[1], form=form, moves=moves)
 
 
@@ -294,6 +303,7 @@ def profile(username):
     result_data = database("profile_data", [result_id])
     room = database("room")
 
+    # Проверяем если есть уже созданная игра
     if room == []:
         state = 0
         apponent = 0
@@ -303,8 +313,7 @@ def profile(username):
     elif result_id == room[1]:
         state = 1
         apponent = room[0]
-        
-        
+
     if request.method == 'POST':
         if form.validate_on_submit():
             apponent = request.form['username']
@@ -323,55 +332,58 @@ def profile(username):
     return render_template('profile.html', win=result_data[0], lose=result_data[1], draw=result_data[2],
                                id_user=result_id, name_user=username, form=form, state=state, apponent=apponent, image=image)
 
+
 @app.route('/facts/<username>')
 def facts(username):
     return render_template('facts.html', username=username)
 
+
 @app.route('/rules/<username>')
 def rules(username):
     return render_template('rules.html', username=username)
+
 
 @app.route('/admin')
 def admin():
     user_info = database("admin")
     return render_template('admin.html', user_info=user_info)
 
+
 @socketio.on("message")
 def handleMessage(data):
-    emit("new_message",data,broadcast=True)
+    emit("new_message", data, broadcast=True)
     add = database("add", [data])
 
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
-    
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/upload_form/<username>', methods=['GET', 'POST'])
 def upload_form(username):
-        if request.method == 'POST':
-            if 'file' not in request.files:
-                flash('Нет файловой части')
-                return render_template('upload.html', username=username)
-            
-            file = request.files['file']
-            
-            if file.filename == '':
-                flash('Не выбрано изображение для загрузки')
-                return render_template('upload.html', username=username)
-                
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                flash('Изображение успешно загружено и отображено ниже')
-                change_pic = database("change_pic", [username, filename])
-                return render_template('upload.html',username=username, filename=filename)
-            else:
-                flash('Допустимые типы изображений: png, jpg, jpeg')
-                return render_template('upload.html', username=username)
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('Нет файловой части')
+            return render_template('upload.html', username=username)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('Не выбрано изображение для загрузки')
+            return render_template('upload.html', username=username)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('Изображение успешно загружено и отображено ниже')
+            change_pic = database("change_pic", [username, filename])
+            return render_template('upload.html', username=username, filename=filename)
         else:
-                return render_template('upload.html', username=username)
-    
+            flash('Допустимые типы изображений: png, jpg, jpeg')
+            return render_template('upload.html', username=username)
+    else:
+        return render_template('upload.html', username=username)
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
@@ -379,5 +391,4 @@ def display_image(filename):
 
 
 if __name__ == "__main__":
-    #socketio.run(app, port=8080, host='127.0.0.1')
     socketio.run(app)
